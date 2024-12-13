8e74ac8eb319b20d40efb45e9e43bb62b05a7e78
Shakaet <abdshakaet@gmail.com>
2024-12-14 02:15:05 +0600

    add

--- src/component/ViewApp.jsx
+++ src/component/ViewApp.jsx
@@ -4,6 +4,29 @@ import { useLoaderData } from 'react-router-dom';
 const ViewApp = () => {
 
     let data= useLoaderData()
+
+    let handleStatusUpdate=(e,id)=>{
+
+        let data=e.target.value
+        console.log(data,id)
+
+        
+        fetch(`http://localhost:3000/job-application/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ status: data }),
+        })
+            .then(res => res.json())
+            .then(data => console.log(data));
+        
+
+
+    }
+
+
+ 
     return (
         <div>
             <h2>{data.length}</h2>
@@ -16,6 +39,8 @@ const ViewApp = () => {
             <th className="border border-gray-300 px-4 py-2">LinkedIn</th>
             <th className="border border-gray-300 px-4 py-2">GitHub</th>
             <th className="border border-gray-300 px-4 py-2">Resume</th>
+             <th className="border border-gray-300 px-4 py-2">Status</th>
+            <th className="border border-gray-300 px-4 py-2">Update Status</th>
           </tr>
         </thead>
         <tbody>
@@ -33,10 +58,32 @@ const ViewApp = () => {
                 </a>
               </td>
               <td className="border border-gray-300 px-4 py-2">
-                <a href={app.resume} target="_blank" rel="noopener noreferrer" className="text-blue-500">
+                <a href={app?.resume} target="_blank" rel="noopener noreferrer" className="text-blue-500">
                   Resume
                 </a>
               </td>
+              <td className="border border-gray-300 px-4 py-2">
+                {app.status}
+              </td>
+
+              <td className="border border-gray-300 px-4 py-2">
+              <div>
+         
+          <select
+          onChange={(e)=>handleStatusUpdate(e,app._id)}
+            name="status" defaultValue={app.status|| "change Status"}
+            className="w-full p-2 border border-gray-300 rounded"
+            
+          >
+            <option value="Under Review">Under Review</option>
+            <option value="Set Review">Set Review</option>
+            <option value="Hired">Hired</option>
+            <option value="Rejected">Rejected</option>
+          </select>
+        </div>
+              </td>
+
+
             </tr>
           ))}
         </tbody>
